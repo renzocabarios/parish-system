fbdd260e57b2039eb71e98b3aeea380e39251189
sairilseb-me <67535165+sairilseb-me@users.noreply.github.com>
2022-08-30 18:02:57 +0800

    Generated migration files for Users, Priests, Baptism, Confirmaton, Marriage

---
+++ database/migrations/2022_08_30_095216_create_confirmations.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confirmations', function (Blueprint $table) {
+            $table->id();
+            $table->text('user_ids');
+            $table->unsignedInteger('priest_id');
+            $table->foreign('priest_id')->references('id')->on('priests');
+            $table->unsignedBigInteger('baptismal_id');
+            $table->foreign('baptismal_id')->references('id')->on('baptismals');
+            $table->text('church');
+            $table->text('church_address');
+            $table->date('confirmed_date');
+            $table->text('sponsors');
+            $table->date('dated');
+            $table->integer('series_of');
+            $table->integer('number');
+            $table->integer('page');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('confirmations');
+    }
+};
