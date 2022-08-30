60809f2d293278cf7d29b15813b999d6f9ac6120
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-30 17:05:09 +0800

    Add LoginView

---
+++ resources/js/views/LoginView.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <div class="h-full w-full flex justify-center items-center text-white">
+        <div class="p-10 bg-sky-800 flex flex-col gap-5 rounded">
+            <div class="flex flex-col">
+                <label for="email">Email</label>
+                <input class="text-black p-2 rounded" type="email" name="email">
+            </div>
+            <div class="flex flex-col">
+                <label for="password">Password</label>
+                <input class="text-black p-2 rounded" type="password" name="password">
+            </div>
+            <button class="px-5 py-2 bg-sky-900">Login</button>
+        </div>
+    </div>
+</template>
