478aec110f0632fb47d334b010397a2c6f9ac8f4
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-30 17:04:42 +0800

    Fix code format

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/css/app.css')
+</head>
 
-       @vite('resources/css/ap.css')
-    </head>
-    <body>
-        <div id="app"></div>
-        @vite('resources/js/app.js')
-    </body>
+<body>
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+</body>
+
 </html>
