d0c1aa26dec844443a2bc473bade61dfc6bde6e0
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-31 13:44:47 +0800

    Add RegisterView

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
 
 const routes = [
     {
         path: "/login",
         name: "Login",
         component: LoginView,
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: RegisterView,
     },
 ];
 
