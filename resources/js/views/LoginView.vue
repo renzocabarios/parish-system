38943e63f0fc87176cfe8aa18821a3d930b7325f
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-31 14:00:21 +0800

    Implement Card component in LoginView

--- resources/js/views/LoginView.vue
+++ resources/js/views/LoginView.vue
@@ -1,14 +1,17 @@
 <script setup>
 import FormInput from '../components/FormInput.vue';
 import PrimaryButton from '../components/PrimaryButton.vue';
+import Card from '../components/Card.vue';
 </script>
 
 <template>
     <div class="h-full w-full flex justify-center items-center text-white">
-        <div class="p-10 bg-sky-800 flex flex-col gap-5 rounded">
-            <FormInput type="email" title="Email" name="email" />
-            <FormInput type="password" title="Password" name="password" />
-            <PrimaryButton title="Register" />
-        </div>
+        <Card>
+            <div class="h-full w-full flex flex-col gap-5">
+                <FormInput type="email" title="Email" name="email" />
+                <FormInput type="password" title="Password" name="password" />
+                <PrimaryButton title="Login" />
+            </div>
+        </Card>
     </div>
 </template>
