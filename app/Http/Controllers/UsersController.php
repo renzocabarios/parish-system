32d36b5f284f6a2ee776c17f811659e7dd3c8845
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-02 19:04:59 +0800

    Add create in UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,4 +20,40 @@ class UsersController extends Controller
         if ($user)  return response()->json(['status' => 'success', 'user' => $user]);
         return response()->json(['status' => 'failed', 'message' => 'Cannot find user.']);
     }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique',
+            'password' => 'required|string',
+            'first_name' => 'required|string',
+            'middle_name' => 'sometimes|string',
+            'last_name' => 'required|string',
+            'date_of_birth' => 'required|date',
+            'fathers_name' => 'required| string',
+            'fathers_citizenship' => 'required | string',
+            'mothers_name' => 'required |string',
+            'mothers_citizenship' => 'required|string',
+            'address' => 'required | string',
+        ]);
+
+        $user = User::create([
+            'id' => Str::uuid(),
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name,
+            'date_of_birth' => $request->date_of_birth,
+            'fathers_name' => $request->fathers_name,
+            'fathers_citizenship' => $request->fathers_citizenship,
+            'mothers_name' => $request->mothers_name,
+            'mothers_citizenship' => $request->mothers_citizenship,
+            'address' => $request->address,
+        ]);
+
+
+        if ($user) return response()->json(['status' => 'success', 'message' => 'Successfully created a user.']);
+        return response()->json(['status' => 'failed', 'message' => 'Failed to create a user.']);
+    }
 }
